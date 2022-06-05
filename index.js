e3732928a40c8496993faf3c34646957ca10d944
malfell <malfellows13@gmail.com>
2022-06-05 13:07:48 -0500

    adding 404 page route

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//configure .env file
+require('dotenv').config();
+
 //Require needed modules
 const express = require('express');
 
@@ -9,5 +12,15 @@ app.get('/', (req, rest) => {
     res.send('Hello world')
 })
 
+//wildcard route that directs to 404
+//this route MUST be on the bottom or it will override other pages!!
+app.get('*', (req, res) => {
+    //chain .status(404) after res to have an official 404 call
+    res.status(404).send(`
+        <h1>404 Page</h1>
+    `)
+})
+
 //listen for connections
-app.listen(3000);
+app.listen(process.env.PORT);
+
