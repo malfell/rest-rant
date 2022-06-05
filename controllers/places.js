59f3875993579f8c9c95b43f304dc3521cab3ac8
malfell <malfellows13@gmail.com>
2022-06-05 14:17:23 -0500

    adding remaining stub routes

--- controllers/places.js
+++ controllers/places.js
@@ -1,20 +1,14 @@
-// //expresses the router. This will hold all the routes.
-// const router = require('express').Router();
+//expresses the router. This will hold all the routes.
+const router = require('express').Router();
 
-// //index page
-// //the GET route will eventually show a list of all places
-// router.get('/', (req, res) => {
-//     res.send('GET /places')
-// })
-
-// //exports the router
-// module.exports = router;
-
-const router = require('express').Router()
-
+//index page
+//the GET route will eventually show a list of all places
 router.get('/', (req, res) => {
     res.send('GET /places')
 })
 
-module.exports = router
+//exports the router
+module.exports = router;
+
+
 
--- index.js
+++ index.js
@@ -1,44 +1,28 @@
-// //configure .env file
-// require('dotenv').config();
+//configure .env file
+require('dotenv').config();
 
-// //Require needed modules
-// const express = require('express');
+//Require needed modules
+const express = require('express');
 
-// //initialize the app object
-// const app = express();
+//initialize the app object
+const app = express();
 
-// //imports the router
-// app.use('/places', require('./controllers/places'))
+//imports the router
+app.use('/places', require('./controllers/places'))
 
-// //make homepage route
-// app.get('/', (req, res) => {
-//     res.send('Hello world')
-// })
-
-// //wildcard route that directs to 404
-// //this route MUST be on the bottom or it will override other pages!!
-// app.get('*', (req, res) => {
-//     //chain .status(404) after res to have an official 404 call
-//     res.status(404).send(`
-//         <h1>404 Page</h1>
-//     `)
-// })
-
-// //listen for connections
-// app.listen(process.env.PORT);
-
-require('dotenv').config()
-const express = require('express')
-const app = express()
-
-app.use('/places', require('./controllers/places))
-
+//make homepage route
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.send('Hello world')
 })
 
+//wildcard route that directs to 404
+//this route MUST be on the bottom or it will override other pages!!
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    //chain .status(404) after res to have an official 404 call
+    res.status(404).send(`
+        <h1>404 Page</h1>
+    `)
 })
 
-app.listen(process.env.PORT)
+//listen for connections
+app.listen(process.env.PORT);
