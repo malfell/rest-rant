792b9ef7a7bb66cb7e22fe62993ec97174bdfc76
malfell <malfellows13@gmail.com>
2022-06-11 15:08:19 -0500

    bonus #2 -- added state dropdown box to the form

--- controllers/places.js
+++ controllers/places.js
@@ -1,27 +1,14 @@
+
+
+//imports places model
+const places = require('../models/places.js')
+
 //expresses the router. This will hold all the routes.
 const router = require('express').Router();
-
-
 
 //index page
 //the GET route will eventually show a list of all places
 router.get('/', (req, res) => {
-    //array for places data
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        // https://unsplash.com/@ninjason Jason Leung
-        pic: '/images/rest-jason-leung.jpg',
-    }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        // https://unsplash.com/@rebaspike Reba Spike (need to include credits)
-        pic: '/images/cat-cafe-reba-spike.jpg',
-    }];
     //render to go into folder and bring up the page
     //since there's another folder inside the views folder, that folder
     //must be referenced
@@ -29,30 +16,35 @@ router.get('/', (req, res) => {
     res.render('places/index', { places })
 })
 
-//NEW Route
-router.get('/new', (req, res) => {
-    res.render('places/new')
-})
-
-// SHOW
-// IF CODE GOES WRONG, SOMETHING LIKE THIS IS NEEDED PROBABLY
-// breads.get('/:arrayIndex', (req, res) => {
-//     if (Bread[req.params.arrayIndex]) {
-//       res.render('Show', {
-//         bread:Bread[req.params.arrayIndex]
-//       })
-//     } else {
-//       res.render('404')
-//     }
-//   })
-
 //CREATE
 //IF SOMETHING GOES WRONG, COME BACK HERE!!! 
 //Refer to Rest-Rant Part 5, end of step 3
 router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST /places')
+    // console.log not needed anymore since code is working,
+    //but I'm keeping it for future reference
+    // console.log(req.body)
+    // Provides a default pic and location
+    if (!req.body.pic) {
+        //Default image if one is not provided
+        req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+        req.body.city = 'Anytown'
+    }
+    if(!req.body.state) {
+        req.body.state = 'USA'
+    }
+    //adds the new data to the places array
+    places.push(req.body)
+    //res.send is changed to res.redirect to redirect to another route
+    //redirects to index route to see newly added place
+    res.redirect('/places')
   })
+
+//NEW Route
+router.get('/new', (req, res) => {
+    res.render('places/new')
+})
 
 //exports the router
 module.exports = router;
---
+++ models/places.js
@@ -0,0 +1,17 @@
+//array for places data
+//array name needs to be changed to module.exports !!!!
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    // https://unsplash.com/@ninjason Jason Leung
+    pic: '/images/rest-jason-leung.jpg',
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    // https://unsplash.com/@rebaspike Reba Spike (need to include credits)
+    pic: '/images/cat-cafe-reba-spike.jpg',
+}];
